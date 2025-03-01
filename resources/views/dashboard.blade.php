23674696ee35703a38fbb09f39b297699189ed31
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2025-03-01 17:20:45 +0400

    Finish Components Section

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,22 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="py-4">
+        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                <div class="p-4 text-gray-900">
+                    <x-category-tabs>
+                        No Categories
+                    </x-category-tabs>
                 </div>
             </div>
+            <div class="mt-8 text-gray-900">
+                @forelse ($posts as $p)
+                    <x-post-item :post="$p"></x-post-item>
+                @empty
+                    <div class="text-center text-gray-400 py-16">No Posts Found</div>
+                @endforelse
+            </div>
+
+            {{ $posts->links() }}
         </div>
     </div>
 </x-app-layout>
