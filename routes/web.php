c4a5d7d4903440303892c6d343be25d2fbf6363d
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2025-03-05 14:33:17 +0400

    Show only posts from followed users

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ Route::get('/', function () {
 Route::get('/@{user:username}', [PublicProfileController::class, 'show'])
     ->name('profile.show');
 
+Route::get('/', [PostController::class, 'index'])
+    ->name('dashboard');
+
+Route::get('/@{username}/{post:slug}', [PostController::class, 'show'])
+    ->name('post.show');
+
 Route::middleware(['auth', 'verified'])->group(function() {
-    Route::get('/', [PostController::class, 'index'])
-        ->name('dashboard');
 
     Route::get('/category/{category}', [PostController::class, 'category'])
         ->name('post.byCategory');
@@ -27,8 +31,6 @@ Route::middleware(['auth', 'verified'])->group(function() {
     Route::post('/post/create', [PostController::class, 'store'])
         ->name('post.store');
 
-    Route::get('/@{username}/{post:slug}', [PostController::class, 'show'])
-        ->name('post.show');
 
     Route::post('/follow/{user}', [FollowerController::class, 'followUnfollow'])
         ->name('follow');
